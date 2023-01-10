e7f0cf4c297727c8f193a95fd24f23d053feeab1
igorjohn <89204391+igorjohn@users.noreply.github.com>
2023-01-10 10:16:39 -0300

    publich Path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: '/pepper-members/',
   chainWebpack: config => {
     config
       .plugin('html')
