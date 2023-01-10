32688ca5f46a3bb00ccc833e87a1fcfe47bbf44b
igorjohn <89204391+igorjohn@users.noreply.github.com>
2023-01-10 11:11:21 -0300

    bug fixed

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed, reactive } from 'vue'
 import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
 
