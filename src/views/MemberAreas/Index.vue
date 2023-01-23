7039a7b7b3591ce89dacc6b86bc03fdfbe0d5b03
igorjohn <89204391+igorjohn@users.noreply.github.com>
2023-01-23 13:36:37 -0300

    Added - modal create memberarea

--- src/views/MemberAreas/Index.vue
+++ src/views/MemberAreas/Index.vue
@@ -1,7 +1,14 @@
 <script setup>
-import { PlusIcon } from '@heroicons/vue/20/solid'
 import { ref } from 'vue'
-import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+import {
+    TransitionRoot,
+    TransitionChild,
+    Dialog,
+    DialogPanel,
+    Menu, MenuButton, MenuItem, MenuItems
+} from '@headlessui/vue'
+
+import { PlusIcon, TrashIcon } from '@heroicons/vue/20/solid'
 
 let showHeaderDropdown = ref(false);
 
@@ -36,10 +43,44 @@ const memberAreas = [
     },
 ]
 
+let isOpenModal = ref(false);
+function openModal() {
+    isOpenModal.value = true;
+}
+
+function closeModal() {
+    isOpenModal.value = false;
+}
+
+
 export default {
+    data: function () {
+        return {
+            pepper: this.pepper,
+            preview: null,
+            image: null
+        };
+    },
 
-
-};
+    methods: {
+        previewImage: function (event) {
+            var input = event.target;
+            if (input.files) {
+                var reader = new FileReader();
+                reader.onload = (e) => {
+                    this.preview = e.target.result;
+                }
+                this.image = input.files[0];
+                console.log(this.image);
+                reader.readAsDataURL(input.files[0]);
+            }
+        },
+        reset: function () {
+            this.image = null;
+            this.preview = null;
+        }
+    }
+}
 </script>
 
 <template>
@@ -104,7 +145,7 @@ export default {
                                 <img class="w-full aspect-video bg-slate-500 object-cover bg-opacity-10"
                                     :src="a.thumbnail" />
                             </div>
-                            <div class="px-1 py-3 font-semibold text-sm mb-2">
+                            <div class="px-1 py-3 font-semibold text-sm mb-2 transition duration-300">
                                 {{ a.title }}
                             </div>
                         </router-link>
@@ -112,7 +153,7 @@ export default {
                 </div>
             </div>
             <div class="flex items-center justify-center mb-14">
-                <button type="button" class="w-full inline-flex justify-center items-center rounded-md border border-pepper-primary bg-pepper-primary px-4 py-3 text-sm font-semibold text-white shadow-sm hover:bg-danger-700 focus:outline-none focus:ring-1 focus:ring-danger-500 focus:ring-offset-1">
+                <button @click="openModal" type="button" :class="pepper.darkMode.button.primary">
                     <PlusIcon class="-ml-1 mr-1 h-4 w-4 font-bold" aria-hidden="true" />
                     <span>Criar nova área de membros</span>
                 </button>
@@ -126,6 +167,120 @@ export default {
             </span>
         </div>
     </div>
+
+
+    <TransitionRoot appear :show="isOpenModal" as="template">
+        <Dialog as="div" @close="closeModal" class="relative z-50">
+            <TransitionChild
+                as="template"
+                enter="duration-300 ease-out"
+                enter-from="opacity-0"
+                enter-to="opacity-100"
+                leave="duration-200 ease-in"
+                leave-from="opacity-100"
+                leave-to="opacity-0">
+                <div class="fixed inset-0 bg-black bg-opacity-50"></div>
+            </TransitionChild>
+            <div class="fixed inset-0 overflow-y-auto">
+                <div class="flex min-h-full items-center justify-center p-4 text-center">
+                    <TransitionChild
+                        as="template"
+                        enter="duration-300 ease-out"
+                        enter-from="opacity-0 scale-95"
+                        enter-to="opacity-100 scale-100"
+                        leave="duration-200 ease-in"
+                        leave-from="opacity-100 scale-100"
+                        leave-to="opacity-0 scale-95">
+                        <DialogPanel class="w-full max-w-2xl transform overflow-hidden rounded-lg bg-white text-left align-middle shadow-xl transition-all overflow-visible">
+                            <div class="m-6">
+                                <h3 class="text-lg font-semibold leading-6 text-gray-900">Criar nova área de membros</h3>
+                                <div class="flex flex-col w-full mx-auto mt-4 gap-4">
+
+                                    <!-- Nome da área de membros -->
+                                    <div>
+                                        <label :class="pepper.darkMode.form.labelWhiteBg">
+                                            Nome da área de membros:
+                                        </label>
+                                        <input
+                                            type="text"
+                                            required
+                                            placeholder="Nome que irá aparecer para seus alunos"
+                                            maxlength="200"
+                                            :class="pepper.darkMode.form.inputWhiteBg" />
+                                    </div>
+
+                                    <!-- Slug -->
+                                    <div>
+                                        <label :class="pepper.darkMode.form.labelWhiteBg">
+                                            Slug (URL) da área de membros:
+                                        </label>
+                                        <input
+                                            type="text"
+                                            required
+                                            placeholder="https://peppermembers.com/slug-da-sua-area"
+                                            maxlength="200"
+                                            :class="pepper.darkMode.form.inputWhiteBg" />
+                                    </div>
+
+                                    <!-- Imagem de capa -->
+                                    <div>
+                                        <label :class="pepper.darkMode.form.labelWhiteBg">
+                                            Imagem de capa:
+                                        </label>
+                                        <label for="file-upload" class="mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 hover:border-indigo-400 transition duration-300 cursor-pointer">
+                                            <div class="space-y-1 text-center bg-white w-full h-full px-6 pt-5 pb-6">
+                                                <!-- input hidden -->
+                                                <input id="file-upload" name="file-upload" type="file" accept="image/*" @change="previewImage" class="sr-only" />
+                                                <!-- upload -->
+                                                <div v-if="!preview" class="w-full">
+                                                    <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+                                                        <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                                    </svg>
+                                                    <div class="flex text-sm text-indigo-500 font-medium justify-center mb-2">
+                                                        Clique para fazer upload
+                                                    </div>
+                                                    <p class="text-xs text-gray-500">PNG, JPG, GIF, WEBP (até 5mb)</p>
+                                                </div>
+                                                <!-- Image preview -->
+                                                <div v-if="preview" class="flex flex-col items-center justify-center">
+                                                    <img
+                                                        style="max-width: 280px"
+                                                        class="aspect-video shadow-lg w-full border border-slate-200 rounded-lg overflow-hidden mx-auto mb-3 max-h-full object-cover bg-slate-500 bg-opacity-10"
+                                                        :src="preview">
+
+                                                    <p class="text-xs text-gray-500 mb-4">{{ image.name }}</p>
+                                                    <button @click="reset" :class="pepper.darkMode.button.primary">
+                                                        <TrashIcon class="h-4 w-4 mr-1" aria-hidden="true" />
+                                                        <span>Remover imagem</span>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </label>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div class="bg-gray-50 rounded-b-lg px-4 py-3 sm:px-6 flex items-center justify-end">
+                                <button
+                                    type="button"
+                                    class="inline-flex justify-center rounded-md border border-transparent bg-gray-200 px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-300"
+                                    @click="closeModal">
+                                    Cancelar
+                                </button>
+                                <button
+                                    type="button"
+                                    class="ml-1 inline-flex justify-center rounded-md border border-transparent bg-pepper-primary px-4 py-2 text-sm font-medium text-white hover:bg-red-700"
+                                    @click="closeModal">
+                                    Confirmar
+                                </button>
+                            </div>
+                        </DialogPanel>
+                    </TransitionChild>
+                </div>
+            </div>
+        </Dialog>
+    </TransitionRoot>
+
 </template>
 
 <style scoped>
