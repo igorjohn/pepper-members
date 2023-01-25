ec19dbd3b9fc2a879bc437803b9f529c6da4af81
igorjohn <89204391+igorjohn@users.noreply.github.com>
2023-01-25 16:35:49 -0300

    margin top fixed

--- src/views/MemberAreas/Area/pages/CreateLesson.vue
+++ src/views/MemberAreas/Area/pages/CreateLesson.vue
@@ -76,7 +76,7 @@ export default {
 
                 <div class="lg:grid lg:grid-cols-4 lg:gap-6">
 
-                    <div class="lg:col-span-1 mb-6">
+                    <div class="lg:col-span-1 mt-2 mb-3 lg:mt-0">
                         <span class="block font-semibold text-base text-white mb-2">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-1 mb-1 text-indigo-500 inline-flex">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
@@ -143,7 +143,7 @@ export default {
                         </div>
                     </div>
 
-                    <div class="lg:col-span-1 mb-6">
+                    <div class="lg:col-span-1 mt-6 mb-3 lg:mt-0">
                         <span class="block font-semibold text-base text-white mb-2">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-1 mb-1 text-indigo-500 inline-flex">
                                 <path stroke-linecap="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"></path>
@@ -211,7 +211,7 @@ export default {
                         </div>
                     </div>
 
-                    <div class="lg:col-span-1 mb-6">
+                    <div class="lg:col-span-1 mt-6 mb-3 lg:mt-0">
                         <span class="block font-semibold text-base text-white mb-2">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-1 mb-1 text-indigo-500 inline-flex">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M18.375 12.739l-7.693 7.693a4.5 4.5 0 01-6.364-6.364l10.94-10.94A3 3 0 1119.5 7.372L8.552 18.32m.009-.01l-.01.01m5.699-9.941l-7.81 7.81a1.5 1.5 0 002.112 2.13" />
