6f9221405d72d2429812d678574595dd510d254a
igorjohn <89204391+igorjohn@users.noreply.github.com>
2023-02-01 18:19:08 -0300

    added - upload images

--- src/views/MemberAreas/Area/pages/Settings.vue
+++ src/views/MemberAreas/Area/pages/Settings.vue
@@ -1,20 +1,43 @@
 <script setup>
-import { ref, computed } from 'vue'
+
+import { TrashIcon, ArrowPathRoundedSquareIcon, Cog6ToothIcon, PhotoIcon } from '@heroicons/vue/24/outline'
+import { InformationCircleIcon } from '@heroicons/vue/20/solid'
+
 </script>
 
 <script type="text/javascript">
 
-import { ref } from 'vue'
 import Header from "@/components/Header.vue";
 
 export default {
     data: function () {
         return {
             pepper: this.pepper,
-            coverFormat: 0
+            coverFormat: 0,
+            preview: null,
+            image: null
+        };
+    },
+
+    methods: {
+        previewImage: function (event) {
+            var input = event.target;
+            if (input.files) {
+                var reader = new FileReader();
+                reader.onload = (e) => {
+                    this.preview = e.target.result;
+                }
+                this.image = input.files[0];
+                console.log(this.image);
+                reader.readAsDataURL(input.files[0]);
+            }
+        },
+        reset: function () {
+            this.image = null;
+            this.preview = null;
         }
     }
-};
+}
 </script>
 
 <template>
@@ -25,12 +48,8 @@ export default {
     <div class="shadow overflow-hidden rounded-md max-w-6xl mx-auto">
         <div class="grid grid-cols-2 gap-4 bg-pepper-dark-2 border border-pepper-dark-3 px-4 pt-5 pb-8 md:px-6 md:pt-6 md:pb-10">
             <div class="col-span-2">
-                <h3 class="flex items-center justify-start gap-x-2 font-semibold text-white text-lg mb-4">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 opacity-50">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z" />
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                    </svg>
+                <h3 class="flex items-center justify-start gap-x-2 font-semibold text-white text-lg mb-2">
+                    <Cog6ToothIcon class="w-5 h-5 opacity-50" />
                     Definições gerais
                 </h3>
             </div>
@@ -77,13 +96,13 @@ export default {
                     :class="pepper.darkMode.form.input" />
             </div>
             <div class="col-span-2 flex items-start gap-x-3 mt-3">
-                <input id="showForPurchase" type="checkbox" class="border-zinc-700 rounded" checked />
+                <input id="allowComments" type="checkbox" class="border-zinc-700 rounded" checked />
                 <div>
-                    <label for="showForPurchase" class="block text-sm text-gray-100 font-medium mb-1 -mt-1 cursor-pointer">
+                    <label for="allowComments" class="block text-sm text-gray-100 font-medium mb-1 -mt-[2px] cursor-pointer">
                         Permitir comentários dos alunos
                     </label>
                     <span class="block text-xs text-gray-400">
-                        Os comentários serão exibidos para aprovação na aba 'Comentários'.
+                        Os comentários serão exibidos para você aprovar e responder, caso queira, na aba 'Comentários'.
                     </span>
                 </div>
             </div>
@@ -95,20 +114,14 @@ export default {
             <!-- Formato da capa dos produtos -->
             <div class="col-span-2">
                 <h3 class="flex items-center justify-start gap-x-2 font-semibold text-white text-lg mt-4 mb-4">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 opacity-50">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 00-3.7-3.7 48.678 48.678 0 00-7.324 0 4.006 4.006 0 00-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3l-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 003.7 3.7 48.656 48.656 0 007.324 0 4.006 4.006 0 003.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3l-3 3" />
-                    </svg>
+                    <ArrowPathRoundedSquareIcon class="w-5 h-5 opacity-50" />
                     Orientação das imagens dos cursos
                 </h3>
 
                 <!-- Alert -->
-                <div class="mt-4 mb-6 bg-slate-700 rounded text-indigo-900 px-4 py-3 lg:py-3.5" role="alert">
+                <div class="mt-4 mb-6 bg-slate-700 bg-opacity-70 rounded text-indigo-900 px-4 py-3 lg:py-3.5" role="alert">
                     <div class="flex">
-                        <div class="py-1">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="h-3.5 w-3.5 fill-current text-slate-400 mr-3" viewBox="0 0 16 16">
-                                <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
-                            </svg>
-                        </div>
+                        <InformationCircleIcon class="h-3.5 w-3.5 fill-current text-slate-400 mr-3 my-1" />
                         <div>
                             <span class="block text-sm text-gray-100 font-medium mb-1.5">
                                 Selecione a aparência da capa dos seus cursos.
@@ -141,24 +154,134 @@ export default {
             </div>
 
             <div class="col-span-2">
-                <h3 class="flex items-center justify-start gap-x-2 font-semibold text-white text-lg mt-4 mb-4">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 opacity-50">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-                    </svg>
-                    Capa da área de membros
+                <h3 class="flex items-center justify-start gap-x-2 font-semibold text-white text-lg mt-4 mb-3">
+                    <PhotoIcon class="w-5 h-5 opacity-50" />
+                    Imagens:
                 </h3>
             </div>
-            <div class="col-span-2 mt-1 flex flex-col items-center justify-center">
-                <span class="inline-block w-72 shadow-lg overflow-hidden rounded-lg border border-zinc-700 bg-pepper-dark-3 aspect-video">
-                    <img v-if="!memberAreaImage" src="https://upload.wikimedia.org/wikipedia/commons/9/95/Silvio_Santos_em_maio_de_2019.jpg" class="h-full w-full object-cover" />
-                </span>
-                <button type="button" class="flex items-center justify-center mt-5 rounded-md border border-zinc-800 bg-pepper-dark-3 py-2 px-3 text-sm font-semibold leading-4 text-indigo-400 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-red-500">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
-                    </svg>
-                    <span>Alterar foto de capa</span>
-                </button>
+
+            <div class="col-span-2 lg:flex lg:gap-5 xl:gap-6">
+                <div class="lg:w-1/3 lg:max-w-[450px]">
+                    <span :class="pepper.darkMode.form.label" class="text-base font-semibold text-gray-200 mb-2">
+                        Capa da área de membros
+                    </span>
+                    <span class="block text-xs text-gray-400 mb-4">
+                        Esta imagem ficará visível para os seus alunos ao acessar a área de membros.
+                    </span>
+                </div>
+                <label for="file-upload" class="mt-1 flex flex-col lg:grow items-center justify-center rounded-md border-2 border-dashed border-zinc-700 hover:border-indigo-600 transition duration-300 cursor-pointer px-6 py-5 text-center bg-zinc-900">
+                    <input id="file-upload" name="file-upload" type="file" accept="image/*" @change="previewImage" class="sr-only" />
+                    <!-- upload -->
+                    <template v-if="!preview">
+                        <svg class="mx-auto h-12 w-12 text-gray-400 -mt-1" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+                            <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                        <div class="flex text-sm text-indigo-500 font-medium justify-center mb-2">
+                            Clique para fazer upload
+                        </div>
+                        <p class="text-xs text-gray-400 mb-1.5">PNG, JPG, GIF, WEBP (até 5mb)</p>
+                        <p class="text-xs text-gray-400">Tamanho sugerido: 1600x900px</p>
+                    </template>
+                    <!-- preview -->
+                    <div v-if="preview" class="flex flex-col items-center justify-center">
+                        <div class="relative bg-zinc-800 max-w-full w-[300px] border border-zinc-700 rounded-md overflow-hidden mx-auto mb-3">
+                            <img class="w-full aspect-video object-cover" :src="preview">
+                            <div class="absolute top-0 right-0 m-2">
+                                <button @click="reset" class="py-1 px-1 rounded bg-slate-600 bg-opacity-40 text-white hover:bg-red-600 hover:bg-opacity-60 transition duration-600">
+                                    <TrashIcon class="h-3 w-3" aria-hidden="true" />
+                                </button>
+                            </div>
+                        </div>
+                        <p class="text-xs text-gray-500 mb-0">{{ image.name }}</p>
+                    </div>
+                </label>
             </div>
+
+            <div class="col-span-2 lg:flex lg:gap-5 xl:gap-6 mt-3">
+                <div class="lg:w-1/3 lg:max-w-[450px]">
+                    <span :class="pepper.darkMode.form.label" class="text-base font-semibold text-gray-200 mb-2">
+                        Logo personalizada
+                    </span>
+                    <span class="block text-xs text-gray-400 mb-4">
+                        Faça o upload da logo da sua marca, para exibi-la no menu lateral.
+                    </span>
+                </div>
+                <label for="file-upload" class="mt-1 flex flex-col lg:grow items-center justify-center rounded-md border-2 border-dashed border-zinc-700 hover:border-indigo-600 transition duration-300 cursor-pointer px-6 py-5 text-center bg-zinc-900">
+                    <input id="file-upload" name="file-upload" type="file" accept="image/*" @change="previewImage" class="sr-only" />
+                    <!-- upload -->
+                    <template v-if="!preview">
+                        <svg class="mx-auto h-12 w-12 text-gray-400 -mt-1" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+                            <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                        <div class="flex text-sm text-indigo-500 font-medium justify-center mb-2">
+                            Clique para fazer upload
+                        </div>
+                        <p class="text-xs text-gray-400">PNG, JPG, GIF, WEBP (até 5mb)</p>
+                    </template>
+                    <!-- preview -->
+                    <div v-if="preview" class="flex flex-col items-center justify-center">
+                        <div class="relative bg-zinc-800 border border-zinc-700 rounded-md overflow-hidden mx-auto mb-3 max-w-full w-[300px] px-3 py-4">
+                            <img class="max-w-[70%] max-h-[26px] mx-auto object-cover" :src="preview">
+                            <div class="absolute top-0 right-0 m-2">
+                                <button @click="reset" class="py-1 px-1 rounded bg-slate-600 bg-opacity-40 text-white hover:bg-red-600 hover:bg-opacity-60 transition duration-600">
+                                    <TrashIcon class="h-3 w-3" aria-hidden="true" />
+                                </button>
+                            </div>
+                        </div>
+                        <p class="text-xs text-gray-500 mb-0">{{ image.name }}</p>
+                    </div>
+                </label>
+            </div>
+
+            <div class="col-span-2 lg:flex lg:gap-5 xl:gap-6 mt-3">
+                <div class="lg:w-1/3 lg:max-w-[450px]">
+                    <span :class="pepper.darkMode.form.label" class="text-base font-semibold text-gray-200 mb-2">
+                        Banner na tela inicial
+                    </span>
+                    <span class="block text-xs text-gray-400 mb-4">
+                        Adicione um banner no topo da tela "Meus conteúdos".
+                    </span>
+                    <div class="flex items-start gap-x-3 mt-5 mb-1.5">
+                        <input id="bannerHref" type="checkbox" class="border-zinc-700 rounded" />
+                        <label for="bannerHref" class="block text-sm text-gray-100 font-medium mb-1 -mt-[2px] cursor-pointer">
+                            Adicionar link no banner (URL externa)
+                        </label>
+                    </div>
+                    <input
+                        type="url"
+                        placeholder="https://seu-link-de-upsell.com.br"
+                        :class="pepper.darkMode.form.input"
+                        class="mb-1 py-2" />
+                </div>
+
+                <label for="file-upload" class="mt-1 flex flex-col lg:grow items-center justify-center rounded-md border-2 border-dashed border-zinc-700 hover:border-indigo-600 transition duration-300 cursor-pointer px-6 py-5 text-center bg-zinc-900">
+                    <input id="file-upload" name="file-upload" type="file" accept="image/*" @change="previewImage" class="sr-only" />
+                    <!-- upload -->
+                    <template v-if="!preview">
+                        <svg class="mx-auto h-12 w-12 text-gray-400 -mt-1" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+                            <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                        <div class="flex text-sm text-indigo-500 font-medium justify-center mb-2">
+                            Clique para fazer upload
+                        </div>
+                        <p class="text-xs text-gray-400 mb-1.5">PNG, JPG, GIF, WEBP (até 5mb)</p>
+                        <p class="text-xs text-gray-400">Tamanho sugerido: 1200x400px</p>
+                    </template>
+                    <!-- preview -->
+                    <div v-if="preview" class="flex flex-col items-center justify-center">
+                        <div class="relative bg-zinc-800 max-w-full w-[300px] border border-zinc-700 rounded-md overflow-hidden mx-auto mb-3">
+                            <img class="w-full" :src="preview">
+                            <div class="absolute top-0 right-0 m-2">
+                                <button @click="reset" class="py-1 px-1 rounded bg-slate-600 bg-opacity-40 text-white hover:bg-red-600 hover:bg-opacity-60 transition duration-600">
+                                    <TrashIcon class="h-3 w-3" aria-hidden="true" />
+                                </button>
+                            </div>
+                        </div>
+                        <p class="text-xs text-gray-500 mb-0">{{ image.name }}</p>
+                    </div>
+                </label>
+            </div>
+
         </div>
         <div class="bg-pepper-dark-3 px-4 py-3 text-right sm:px-6">
             <button
